240c70766f818bc7c3bf271ca045663ab4a85fea
Donghee Park <dongheepark@gmail.com>
2023-10-19 11:35:17 +0900

    Add result figure

--- web/app.py
+++ web/app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 import random
+
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from simulation_modify_2 import safety_fig, safety
 
 app = Flask(__name__)
 
@@ -10,6 +14,17 @@ def index():
 @app.route('/wearability')
 def wearability():
     return f'<h4>{random.randint(80, 100)}</h4>'
+
+@app.route('/fig/safety')
+def safety_img():
+    img = safety_fig(baselink_angle=0, device_angle=175)
+    return send_file(img, mimetype='image/png')
+
+@app.route('/safety')
+def safety_txt():
+    result = safety(baselink_angle=0, device_angle=175)
+    return f'<h4>{result[0]}, {result[1]}, {result[2]}, {result[3]}, {result[4]}</h4>'
+
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=5052)
